3aa54bc888966e54567c80a296f886a2caacd0f1
merlinlim <merlinlim98@gmail.com>
2023-01-19 17:50:56 +0800

    Change Score message

--- components/score.js
+++ components/score.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 const Score = ({nought_score, circle_score}) => {
   return (
-    <div className="w-24 md:w-40 h-10 flex flex-col justify-center
-                    md:text-xl text-center rounded-lg shadow-xl bg-red-600
-                     text-white">
+    <div className="w-24 md:w-48 h-16 flex flex-col justify-center
+                    md:text-xl text-center rounded-2xl shadow-xl 
+                    bg-red-600 text-white">
         <div className="flex justify-evenly">
+            <div>Score: </div>
             <div>{nought_score}</div>
             <div> - </div>
             <div>{circle_score}</div>
